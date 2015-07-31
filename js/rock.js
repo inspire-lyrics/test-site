4a0b7b5737e4a50ad44486a3c10abc71737e169b
inspire-lyrics <ladyibarra@students.berkeley.net>
2015-07-31 12:19:41 -0700

    First Push

---
+++ js/rock.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $(".album-title").click(function() {
+     var addheight = $(this).parent().find(".song-list").height()-75;
+     $(this).parent().find(".song-list").slideToggle("slow");
+     var flag = $(this).data('flag');
+     $(this).parent().parent().animate({height: flag ? '450' : '+='+addheight}, 100);
+     $(this).data('flag', !flag);
+   });
+});
